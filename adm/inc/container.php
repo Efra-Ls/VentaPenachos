448b513a0ef9d025c68fa5bf556234afcde70f19
Efra-Ls <104372088+Efra-Ls@users.noreply.github.com>
2023-04-22 18:11:44 -0600

    Add files via upload

---
+++ adm/inc/container.php
@@ -0,0 +1,12 @@
+</head>
+
+<body class="bg-gradient bg-warning bg-opacity-100">
+  <nav role="navigation" class="navbar navbar-dark navbar-static-top bg-danger bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="https://www.configuroweb.com/46-aplicaciones-gratuitas-en-php-python-y-javascript/#Aplicaciones-gratuitas-en-PHP,-Python-y-Javascript" target="_blank" class="navbar-brand">Arte plumario</a>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
---
+++ adm/inc/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <h3 class="text-center my-4 py-3 text-light" id="title">Derechos recervados 2023 - Arte plumario</h3>
+
+    
+</footer>
